09c09cebb1461a85bb98083603f02026c2cd3ade
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2026-04-15 00:00:27 -0300

    feat: Incluindo StyledComponents

--- myFinancesSystem/src/components/Balance/Balance.tsx
+++ myFinancesSystem/src/components/Balance/Balance.tsx
@@ -1,11 +1,96 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { useState } from "react";
-import type { BalanceProps } from "../../models/interfaces/BalanceProps/BalanceProps";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollar } from "@fortawesome/free-solid-svg-icons";
-import "./Balance.css";
-import Button from "../Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import styled from "styled-components";
+import type { BalanceProps } from "../../models/interfaces/BalanceProps/BalanceProps";
 import { FormatMoney } from "../../utils/utils";
+import Button from "../Button/Button";
+import "./Balance.css";
+import type { FormContainerProps } from "../../models/interfaces/FormContainerProps/FormContainerProps";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Card = styled.div`
+  background-color: #36383e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 500px;
+  height: 300px;
+  border-radius: 1.2rem;
+
+  & h2 {
+    margin-left: 1rem;
+    font-weight: 500;
+    font-size: 2.2rem;
+    color: #dddcda;
+  }
+
+  & h3 {
+    margin-left: 1rem;
+    font-weight: 500;
+    font-size: 2.2rem;
+    color: #dddcda;
+  }
+`;
+
+export const CardHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -2rem;
+`;
+
+export const FormContainer = styled.div<FormContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+
+  & input {
+    background: ${(props: FormContainerProps) =>
+      props.invalid ? "#e43f4d7a" : "transparent"};
+    box-shadow: ${(props: FormContainerProps) =>
+      props.invalid ? "inset #e43f4d 0 0 0 2px" : ""};
+  }
+`;
+
+export const FormInput = styled.input`
+  box-shadow: inset #dddcda 0 0 0 2px;
+  border: 0;
+  background: rgba(0, 0, 0, 0);
+  appearance: none;
+  width: 50%;
+  position: relative;
+  border-radius: 10px;
+  padding: 9px 12px;
+  line-height: 1.4;
+  color: #dddcda;
+  font-size: 16px;
+  font-weight: 400;
+  height: 30px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    box-shadow:
+      0 0 0 0 #fff inset,
+      #7af1a7 0 0 0 2px;
+  }
+
+  &:focus {
+    background: #ffffff00;
+    outline: 0;
+    box-shadow:
+      0 0 0 0 #fff inset,
+      #7af1a7 0 0 0 3px;
+  }
+`;
 
 const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
   const [renderInputForm, setRenderInputForm] = useState(false);
@@ -54,12 +139,12 @@ const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
 
   return (
     <div>
-      <div className="balance_container">
-        <div className="balance_card">
-          <header className="balance_header">
+      <Container>
+        <Card>
+          <CardHeader>
             <FontAwesomeIcon icon={faDollar} color="#7af1a7" size="2x" />
             <h2>Saldo</h2>
-          </header>
+          </CardHeader>
 
           <h3>
             {currentBalance > 0
@@ -77,24 +162,20 @@ const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
 
           {renderInputForm && (
             <form onSubmit={formSubmitHandler}>
-              <div
-                className={`input_form_container ${isFormValid} "invalid" : ""}`}
-              >
-                <input
+              <FormContainer invalid={!isFormValid}>
+                <FormInput
                   type="text"
                   placeholder="Nome"
-                  className="balance_input"
                   value={inputName}
                   onChange={handleInputNameForm}
                 />
-                <input
+                <FormInput
                   type="text"
                   placeholder="Valor"
-                  className="balance_input"
                   value={inputValue}
                   onChange={handleInputValueForm}
                 />
-              </div>
+              </FormContainer>
 
               <div className="actions_form_button_container">
                 <Button
@@ -106,8 +187,8 @@ const Balance = ({ emitMovement, currentBalance }: BalanceProps) => {
               </div>
             </form>
           )}
-        </div>
-      </div>
+        </Card>
+      </Container>
     </div>
   );
 };
